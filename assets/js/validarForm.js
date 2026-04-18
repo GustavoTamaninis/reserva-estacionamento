bf47e4f76cb9037b71595534ae654177bb947dee
GustavoTamaninis <194152443+GustavoTamaninis@users.noreply.github.com>
2026-04-18 20:28:20 -0300

    feat: reserva de vagas

---
+++ assets/js/validarForm.js
@@ -0,0 +1,53 @@
+function validarForm(){
+
+    const cxPlaca = document.getElementById("placa");
+    const cxNome = document.getElementById("nome");
+    const cxAptoNum = document.getElementById("aptoNum");
+    const cxAptoBloco = document.getElementById("aptoBloco");
+    const cxModelo = document.getElementById("modelo");
+    const cxCor = document.getElementById("cor");
+    const cxVaga = document.getElementById("vaga");
+
+    const placa = cxPlaca.value.trim();
+    const nome = cxNome.value.trim();
+    const aptoNum = cxAptoNum.value.trim();
+    const aptoBloco = cxAptoBloco.value.trim();
+    const modelo = cxModelo.value.trim();
+    const cor = cxCor.value.trim();
+    const vaga = cxVaga.value.trim();
+
+    const regexPlaca = /^[A-Z]{3}-?\d{4}$|^[A-Z]{3}\d[A-Z]\d{2}$/i;
+    const regexNome = /^[a-záàâãéèêíïóôõöúçñ\s]{4,40}$/i;
+    const regexAptoNum = /^\d{1,4}$/;
+    const regexAptoBloco = /^[a-z0-9]{1,10}$/i;
+    const regexModelo = /^[a-z0-9\s\-]{2,40}$/i;
+    const regexCor = /^[a-záàâãéèêíïóôõöúçñ\s]{3,20}$/i;
+    const regexVaga = /^\d{1,3}$/;
+
+    let c = localStorage.getItem("contador");
+    if(c){
+        localStorage.setItem("contador", parseInt(c) + 1);
+    }else{
+        localStorage.setItem("contador", 1);
+    }
+
+    if(!validarCampo(cxPlaca, "placa", placa, regexPlaca, "Placa do Veículo")) return false;
+    if(!validarCampo(cxNome, "nome", nome, regexNome, "Nome do Proprietário")) return false;
+    if(!validarCampo(cxAptoNum, "aptNum", aptoNum, regexAptoNum, "Número do Apartamento")) return false;
+    if(!validarCampo(cxAptoBloco, "aptoBloco", aptoBloco, regexAptoBloco, "Bloco do apartamento")) return false;
+    if(!validarCampo(cxModelo, "modelo", modelo, regexModelo, "Modelo do Veículo")) return false;
+    if(!validarCampo(cxCor, "cor", cor, regexCor, "Cor do Veículo")) return false;
+    if(!validarCampo(cxVaga, "vaga", vaga, regexVaga, "Número da Vaga de Estacionamento")) return false;
+    return true;
+}
+
+function validarCampo(cx, chave, valor, regex, mensagem){
+    if(valor == "" || !regex.test(valor)){
+        alert("Erro! Preencha corretamente o campo " + mensagem + "!");
+        cx.focus();
+        return false;
+    }
+    console.log("Armazenado " + valor + "em " + mensagem);
+    localStorage.setItem(chave + localStorage.getItem("contador").toString(), valor);
+    return true;
+}
